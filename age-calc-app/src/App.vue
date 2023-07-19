d7e1f9eb606a40331539caae1d8ca8fac003222f
clairemcintosh88 <claire.mcintosh@rapp.com>
2023-07-19 15:28:13 +0100

    updates

--- age-calc-app/src/App.vue
+++ age-calc-app/src/App.vue
@@ -5,14 +5,10 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
+    <div class="container mx-auto">
       <nav>
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/agecalc">Age Calc</RouterLink>
       </nav>
     </div>
   </header>
---
+++ age-calc-app/src/views/AgecalcView.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="agecalc container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
+    <h1 class="text-3xl font-bold">This is the age calc page</h1>
+  </div>
+</template>
+
